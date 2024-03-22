0e1674c9d14176bc18316a9e4aa59f2f424b4c2f
Masato Yamamoto <masayama@c1r21s9.42tokyo.jp>
2024-03-22 21:06:09 +0900

    ege

--- C12/ex00/ft_create_elem.c
+++ C12/ex00/ft_create_elem.c
@@ -6,7 +6,7 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 10:12:08 by masayama          #+#    #+#             */
-/*   Updated: 2024/03/22 10:38:50 by masayama         ###   ########.fr       */
+/*   Updated: 2024/03/22 20:47:18 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,10 @@
 
 t_list	*ft_create_elem(void *data)
 {
-	struct s_list *list;
-	list = malloc(sizeof(t_list) * 2);
+	struct s_list	*list;
+
+	list = malloc(sizeof(t_list) * 1);
 	list->next = NULL;
 	list->data = data;
 	return (list);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	t_list *a;
-	a = ft_create_elem("aa");
-	printf("s\n", a->next);
-	printf("s\n", a->next);
 }
--- C12/ex00/ft_list.h
+++ C12/ex00/ft_list.h
@@ -6,12 +6,17 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 10:11:22 by masayama          #+#    #+#             */
-/*   Updated: 2024/03/22 10:11:32 by masayama         ###   ########.fr       */
+/*   Updated: 2024/03/22 21:05:41 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
 
 typedef struct s_list
 {
 	struct s_list	*next;
 	void			*data;
 }					t_list;
+
+#endif
