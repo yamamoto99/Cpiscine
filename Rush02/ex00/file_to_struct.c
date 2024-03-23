6eb2192fc01fbe1846c01a804740876c1ff8fb92
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-03-23 16:06:27 +0900

    geg

--- Rush02/ex00/file_to_struct.c
+++ Rush02/ex00/file_to_struct.c
@@ -6,11 +6,12 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 09:51:18 by masayama          #+#    #+#             */
-/*   Updated: 2024/03/23 11:13:38 by masayama         ###   ########.fr       */
+/*   Updated: 2024/03/23 11:56:52 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include "rush_02.h"
 
@@ -18,8 +19,9 @@ int	file_to_struct(void)
 {
 	int	fd;
 	char buf[1];
-	// ssize_t n;
+	ssize_t n;
 	int	count;
+	char *str;
 	
 	fd = open("numbers.dict", O_RDONLY);
 	if (fd == -1){
@@ -28,8 +30,14 @@ int	file_to_struct(void)
 	}
 	count = 0;
 	while (read(fd, buf, 1))
+	{
 		count++;
+	}
+	str = malloc(sizeof(char) * count + 1);
+	n = read(fd, str, count);
+	if(n == -1)
+		return (-1);
 	close(fd);
-	ft_putnbr(count);
+	ft_putstr(str);
 	return (1);
 }
--- Rush02/ex00/rush_02.h
+++ Rush02/ex00/rush_02.h
@@ -6,7 +6,7 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 07:40:28 by masayama          #+#    #+#             */
-/*   Updated: 2024/03/23 11:17:33 by masayama         ###   ########.fr       */
+/*   Updated: 2024/03/23 16:05:56 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,9 @@
 
 typedef struct s_dict
 {
-	char			*key;
-	char			*nbr;
-	struct s_dict	*next;
-}					t_dict;
-
-typedef struct s_dict_list
-{
-	t_dict	*head;
-}			t_dict_list;
+	char	*key;
+	char	*value;
+}			t_dict;
 
 void	ft_putstr(char *str);
 int		file_to_struct(void);
