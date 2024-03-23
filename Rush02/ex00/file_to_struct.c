81ffce6a7dcca9153982155499da332e19890c19
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-03-23 09:56:34 +0900

    feat:LinkedList

--- Rush02/ex00/file_to_struct.c
+++ Rush02/ex00/file_to_struct.c
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush_02.h                                          :+:      :+:    :+:   */
+/*   file_to_struct.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/23 07:40:28 by masayama          #+#    #+#             */
-/*   Updated: 2024/03/23 09:54:43 by masayama         ###   ########.fr       */
+/*   Created: 2024/03/23 09:51:18 by masayama          #+#    #+#             */
+/*   Updated: 2024/03/23 09:51:19 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RUSH_02_H
-# define RUSH_02_H
-
-typedef struct s_dict
-{
-	char			*key;
-	char			*nbr;
-	struct s_dict	*next;
-	struct s_dict	*prev;
-}					t_dict;
-
-typedef struct s_dict_list
-{
-	t_dict	*head;
-	t_dict	*tail;
-}			t_dict_list;
-
-void	ft_putstr(char *str);
-
-#endif
