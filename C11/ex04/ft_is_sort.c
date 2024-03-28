ca6e1b2ca05e81373cfc0e6de50fc5f916a781c5
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-03-28 15:51:55 +0900

    graggr

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -6,36 +6,46 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 14:56:07 by masayama          #+#    #+#             */
-/*   Updated: 2024/03/22 20:22:31 by masayama         ###   ########.fr       */
+/*   Updated: 2024/03/27 11:06:03 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// int	is_sort(int a, int b)
-// {
-// 	if (a == b)
-// 		return (0);
-// 	else
-// 		return (a - b);
-// }
+int	is_sort(int a, int b)
+{
+	if (a == b)
+		return (0);
+	else
+		return (a - b);
+}
 
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
 	int	i;
+	int	flag;
 
+	flag = 0;
 	i = 0;
+	while (tab[i] == tab[i + 1])
+		i++;
+	if (f(tab[i], tab[i + 1]) > 0)
+		flag = 1;
+	else
+		flag = -1;
 	while (i < length - 1)
 	{
-		if (f(tab[i], tab[i + 1]) > 0)
+		if (flag == 1 && f(tab[i], tab[i + 1]) < 0)
+			return (0);
+		else if (flag == -1 && f(tab[i], tab[i + 1]) > 0)
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int	main(void)
-// {
-// 	int	a[] = {1, 2, 3, 4, 5};
-// 	printf("%d\n", ft_is_sort(a, 5, &is_sort));
-// }
+int	main(void)
+{
+	int	a[] = {5, 4, 3, 2, 1};
+	printf("%d\n", ft_is_sort(a, 5, &is_sort));
+}
